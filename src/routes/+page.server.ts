2888676017820744efe0556757671e59dc07a9a9
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-10-25 23:18:42 -0400

    Added feature flag suuport via Vercel Toolbar.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,20 @@
+import { showDashboard } from '$lib/flags';
+
 const delay = (amt: number) => new Promise(resolve => setTimeout(resolve, amt));
 
 
 export const load = async ({ fetch, params, parent, url }) => {
 
+  const dashboard = await showDashboard();
+
   const myFunction = async () => {
-    await delay(1000);
-    return new Date();
+    await delay(1001);
+    console.log('RENDERED');
+    return new Date().toISOString();
   }
 
   return {
-    renderDate: await myFunction()
+    renderDate: await myFunction(),
+    dashboard
   }
 }
