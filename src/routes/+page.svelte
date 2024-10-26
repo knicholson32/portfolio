bf1daa4665ed27208a7f4a5f1c6354116b0bcd18
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-10-25 23:45:13 -0400

    Restored feature flag backend because Vercel is expecting the well known endpoint.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Rendered on: {data.renderDate}</p>
-<p>{data.dashboard}</p>
