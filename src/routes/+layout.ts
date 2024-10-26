110dd4fc93e63e6aa8d59d8b32f7cc2b00aafde8
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-10-25 22:59:44 -0400

    Migrated to Svelte 5. Included playwright, storybook, tailwindcss and others.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,4 @@
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-import { dev } from '$app/environment'; 
-
-
 
 export const prerender = true;
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,14 @@
-const delay = (amt: number) =>  new Promise(resolve => setTimeout(resolve, amt));
+const delay = (amt: number) => new Promise(resolve => setTimeout(resolve, amt));
 
 
 export const load = async ({ fetch, params, parent, url }) => {
 
   const myFunction = async () => {
-    await delay (1000);
-    return ('Render time:' + new Date());
+    await delay(1000);
+    return new Date();
   }
 
   return {
-    async: {
-      value: await myFunction()
-    }
+    renderDate: await myFunction()
   }
 }
