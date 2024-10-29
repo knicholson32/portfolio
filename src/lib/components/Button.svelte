ab65526d97a9c8751506f12d5605889055fd30ec
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-10-29 00:04:02 -0400

    Removed Storybook because it does not properly work with Svelte 5 yet. Implemented a basic API test and a 404 page.

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+
+  // /**
+  //  * @typedef {Object} Props
+  //  * @property {boolean} [primary] Is this the principal call to action on the page?
+  //  * @property {string} [backgroundColor] What background color to use
+  //  * @property {'small' | 'medium' | 'large'} [size] How large should the button be?
+  //  * @property {string} label Button contents
+  //  * @property {() => void} [onClick] The onclick event handler
+  //  */
+
+  export let backgroundColor: string;
+  export let label: string;
+  export let onClick: () => void;
+  
+  export let primary = false;
+  export let size: 'small' | 'medium' | 'large' = 'medium';
+</script>
+
+<style>
+  .storybook-button {
+    display: inline-block;
+    cursor: pointer;
+    border: 0;
+    border-radius: 3em;
+    font-weight: 700;
+    line-height: 1;
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+  .storybook-button--primary {
+    background-color: #1ea7fd;
+    color: white;
+  }
+  .storybook-button--secondary {
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    background-color: transparent;
+    color: #333;
+  }
+  .storybook-button--small {
+    padding: 10px 16px;
+    font-size: 12px;
+  }
+  .storybook-button--medium {
+    padding: 11px 20px;
+    font-size: 14px;
+  }
+  .storybook-button--large {
+    padding: 12px 24px;
+    font-size: 16px;
+  }
+</style>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`].join(' ')}
+  class:storybook-button--primary={primary}
+  class:storybook-button--secondary={!primary}
+  style:background-color={backgroundColor}
+  onclick={onClick}
+>
+  {label}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
+  import Button from "$lib/components/Button.svelte";
+  import { onMount } from "svelte";
+
+  let myWords: Promise<[string, string]> = new Promise((_resolve, _reject) => {  });
+  const getWords = async () => {
+    myWords = new Promise((_resolve, _reject) => {  });
+    myWords = (await fetch('https://random-word-api.herokuapp.com/word?number=2')).json() as unknown as Promise<[string, string]>;
+  }
 
   export let data;
+
+  onMount(() => {
+    getWords();
+  });
 
 </script>
 
 <h1>Welcome to SvelteKit!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Rendered on: {data.renderDate}</p>
+
+
+<div class="inline-flex gap-2 items-center">
+  <Button onClick={getWords} label="New Words" backgroundColor="#ffffff" />
+  {#each data.words as word}
+    <span>{word}</span>
+  {/each}
+
+  {#await myWords}
+    <span class="text-xs text-gray-500 italic lowercase">Loading</span>
+  {:then words} 
+    {#each words as word}
+      <span class="animate-bounce">{word}</span>
+    {/each}
+  {/await}
+</div>
