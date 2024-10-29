e69bab4aa6a8b59eeb94059fbf3677d267f53147
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-10-28 22:13:09 -0400

    Initial changes to move to Cloudflare Pages.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-  import { onMount } from 'svelte';
-	
-  import { mountVercelToolbar } from '@vercel/toolbar/vite';
-  onMount(() => mountVercelToolbar());
- 
- 
+
 	let { children } = $props();
 </script>
 
