7ee6f7a032a3253e2a7baf3d84cd350fc4c50609
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-11-12 18:17:11 -0500

    Added initial logo and improved the OpenGraph component.

--- src/lib/components/generic/OpenGraph.svelte
+++ src/lib/components/generic/OpenGraph.svelte
@@ -16,6 +16,7 @@
 <svelte:head>
   <meta property="og:title" content={data.title}>
   <meta property="og:description" content={data.description}>
+  <meta property="og:logo" content="logo.png">
   {#if data.image !== undefined}
     <meta property="og:image" content={data.image}>
   {/if}
--- src/routes/post/+layout.svelte
+++ src/routes/post/+layout.svelte
@@ -12,7 +12,7 @@
 
 {#if data.slug !== null && data.meta !== null}
   <Head title={data.meta.item.title} description={data.meta.item.summary ?? ''}/>
-  <OpenGraph title={data.meta.item.title} description={data.meta.item.summary ?? 'No description provided.'} slug={data.slug}/>
+  <OpenGraph title={data.meta.item.title} description={data.meta.item.summary ?? 'No description provided.'} slug={data.slug} image={data.meta.item.image}/>
 {/if}
 
 {@render children?.()}
