c5b10638d387d40989d811834b50bd0941da0860
Keenan Nicholson <keenanrnicholson@gmail.com>
2024-11-13 12:52:31 -0500

    Moved deploy URLs to repo variables. Updated README.md.

--- src/routes/post/+layout.svelte
+++ src/routes/post/+layout.svelte
@@ -12,7 +12,7 @@
 
 {#if data.slug !== null && data.meta !== null}
   <Head title={data.meta.item.title} description={data.meta.item.summary ?? ''}/>
-  <OpenGraph title={data.meta.item.title} description={data.meta.item.summary ?? 'No description provided.'} slug={data.slug} image={data.meta.item.image}/>
+  <OpenGraph title={data.meta.item.title} description={data.meta.item.summary ?? 'No description provided.'} slug={data.meta.item.id} image={data.meta.item.image}/>
 {/if}
 
 {@render children?.()}
